3c8a17060aa98f1167b6564fa5a2f85fc7cadce9
Alex Rosenbach <ajrosenbach@yahoo.com>
2021-05-30 01:39:11 -0400

    Added project files

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import 'bulma/css/bulma.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <section class="hero">
+        <div class="hero-body">
+          <p class="title">
+            The Town Halls of Massachusetts
+      </p>
+          <p class="subtitle">
+            351 Town Halls from the Berkshires to the Cape
+      </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TownHallsView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TownHallsView = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TownHallsView
